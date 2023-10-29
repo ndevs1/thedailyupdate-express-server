e0b7d234c9c6b8937760ce80751ed9820afc29a4
nahidthenh <nhgroup18@gmail.com>
2023-10-29 11:37:46 +0600

    serverless issue

--- index.js
+++ index.js
@@ -1,74 +1,492 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = process.env.PORT || 5000
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-app.use(cors())
+// Your JSON data
+const jsonData = {
+    "menu": [
+        {
+            "id": 1,
+            "name": "Home",
+            "slug": "home",
+            "submenus": []
+        },
+        {
+            "id": 2,
+            "name": "News",
+            "slug": "news",
+            "submenus": [
+                {
+                    "id": 3,
+                    "name": "Politics",
+                    "slug": "politics"
+                },
+                {
+                    "id": 4,
+                    "name": "Business",
+                    "slug": "business"
+                }
+            ]
+        },
+        {
+            "id": 5,
+            "name": "Sports",
+            "slug": "sports",
+            "submenus": []
+        }
+    ],
+    "newsCategories": [
+        {
+            "id": 1,
+            "name": "Politics",
+            "slug": "politics",
+            "color": "#FF5733"
+        },
+        {
+            "id": 2,
+            "name": "Business",
+            "slug": "business",
+            "color": "#337CFF"
+        }
+    ],
+    "allNews": [
+        {
+            "id": 1,
+            "title": "allNews News Title 1",
+            "shortDescription": "Short description for the popular news article 1",
+            "loginDescription": "Detailed description for the popular news article 1",
+            "image": "popular_image_1.jpg",
+            "timeToRead": "5 min",
+            "category": "Politics",
+            "categorySlug": "politics",
+            "author": "John Doe"
+        },
+        {
+            "id": 2,
+            "title": "allNews News Title 2",
+            "shortDescription": "Short description for the popular news article 2",
+            "loginDescription": "Detailed description for the popular news article 2",
+            "image": "popular_image_2.jpg",
+            "timeToRead": "7 min",
+            "category": "Business",
+            "categorySlug": "business",
+            "author": "Jane Smith"
+        },
+        {
+            "id": 3,
+            "title": "allNews News Title 3",
+            "shortDescription": "Short description for the popular news article 3",
+            "loginDescription": "Detailed description for the popular news article 3",
+            "image": "popular_image_2.jpg",
+            "timeToRead": "7 min",
+            "category": "Business",
+            "categorySlug": "business",
+            "author": "Jane Smith"
+        },
+        {
+            "id": 4,
+            "title": "allNews News Title 4",
+            "shortDescription": "Short description for the popular news article 4",
+            "loginDescription": "Detailed description for the popular news article 4",
+            "image": "popular_image_2.jpg",
+            "timeToRead": "7 min",
+            "category": "Business",
+            "categorySlug": "business",
+            "author": "Jane Smith"
+        }
+    ],
+    "featuresNews": [
+        {
+            "id": 1,
+            "category": "বাণিজ্য",
+            "title": "আয়কর রিটার্ন জমা নভেম্বরের মধ্যে",
+            "author": "admin",
+            "image": " news/3.jpg",
+            "catbgColor": "red"
+        },
+        {
+            "id": 1,
+            "title": "featuresNews News Title 1",
+            "shortDescription": "Short description for the popular news article 1",
+            "loginDescription": "Detailed description for the popular news article 1",
+            "image": "popular_image_1.jpg",
+            "timeToRead": "5 min",
+            "category": "Politics",
+            "categorySlug": "politics",
+            "author": "John Doe"
+        },
+        {
+            "id": 2,
+            "title": "featuresNews News Title 2",
+            "shortDescription": "Short description for the popular news article 2",
+            "loginDescription": "Detailed description for the popular news article 2",
+            "image": "popular_image_2.jpg",
+            "timeToRead": "7 min",
+            "category": "Business",
+            "categorySlug": "business",
+            "author": "Jane Smith"
+        },
+        {
+            "id": 3,
+            "title": "featuresNews News Title 3",
+            "shortDescription": "Short description for the popular news article 3",
+            "loginDescription": "Detailed description for the popular news article 3",
+            "image": "popular_image_2.jpg",
+            "timeToRead": "7 min",
+            "category": "Business",
+            "categorySlug": "business",
+            "author": "Jane Smith"
+        },
+        {
+            "id": 4,
+            "title": "featuresNews News Title 4",
+            "shortDescription": "Short description for the popular news article 4",
+            "loginDescription": "Detailed description for the popular news article 4",
+            "image": "popular_image_2.jpg",
+            "timeToRead": "7 min",
+            "category": "Business",
+            "categorySlug": "business",
+            "author": "Jane Smith"
+        },
+        {
+            "id": 5,
+            "title": "featuresNews News Title 5",
+            "shortDescription": "Short description for the popular news article 5",
+            "loginDescription": "Detailed description for the popular news article 5",
+            "image": "popular_image_2.jpg",
+            "timeToRead": "7 min",
+            "category": "Business",
+            "categorySlug": "business",
+            "author": "Jane Smith"
+        },
+        {
+            "id": 6,
+            "title": "featuresNews News Title 6",
+            "shortDescription": "Short description for the popular news article 6",
+            "loginDescription": "Detailed description for the popular news article 6",
+            "image": "popular_image_2.jpg",
+            "timeToRead": "7 min",
+            "category": "Business",
+            "categorySlug": "business",
+            "author": "Jane Smith"
+        }
+    ],
+    "popularNews": [
+        {
+            "id": 1,
+            "title": "Popular News Title 1",
+            "shortDescription": "Short description for the popular news article 1",
+            "loginDescription": "Detailed description for the popular news article 1",
+            "image": "popular_image_1.jpg",
+            "timeToRead": "5 min",
+            "category": "Politics",
+            "categorySlug": "politics",
+            "author": "John Doe"
+        },
+        {
+            "id": 2,
+            "title": "Popular News Title 2",
+            "shortDescription": "Short description for the popular news article 2",
+            "loginDescription": "Detailed description for the popular news article 2",
+            "image": "popular_image_2.jpg",
+            "timeToRead": "7 min",
+            "category": "Business",
+            "categorySlug": "business",
+            "author": "Jane Smith"
+        },
+        {
+            "id": 3,
+            "title": "Popular News Title 3",
+            "shortDescription": "Short description for the popular news article 3",
+            "loginDescription": "Detailed description for the popular news article 3",
+            "image": "popular_image_2.jpg",
+            "timeToRead": "7 min",
+            "category": "Business",
+            "categorySlug": "business",
+            "author": "Jane Smith"
+        },
+        {
+            "id": 4,
+            "title": "Popular News Title 4",
+            "shortDescription": "Short description for the popular news article 4",
+            "loginDescription": "Detailed description for the popular news article 4",
+            "image": "popular_image_2.jpg",
+            "timeToRead": "7 min",
+            "category": "Business",
+            "categorySlug": "business",
+            "author": "Jane Smith"
+        }
+    ],
+    "recentNews": [
+        {
+            "id": 1,
+            "title": "RecentNews News Title 1",
+            "shortDescription": "Short description for the popular news article 1",
+            "loginDescription": "Detailed description for the popular news article 1",
+            "image": "popular_image_1.jpg",
+            "timeToRead": "5 min",
+            "category": "Politics",
+            "categorySlug": "politics",
+            "author": "John Doe"
+        },
+        {
+            "id": 2,
+            "title": "RecentNews News Title 2",
+            "shortDescription": "Short description for the popular news article 2",
+            "loginDescription": "Detailed description for the popular news article 2",
+            "image": "popular_image_2.jpg",
+            "timeToRead": "7 min",
+            "category": "Business",
+            "categorySlug": "business",
+            "author": "Jane Smith"
+        },
+        {
+            "id": 3,
+            "title": "RecentNews News Title 3",
+            "shortDescription": "Short description for the popular news article 3",
+            "loginDescription": "Detailed description for the popular news article 3",
+            "image": "popular_image_2.jpg",
+            "timeToRead": "7 min",
+            "category": "Business",
+            "categorySlug": "business",
+            "author": "Jane Smith"
+        },
+        {
+            "id": 4,
+            "title": "RecentNews News Title 4",
+            "shortDescription": "Short description for the popular news article 4",
+            "loginDescription": "Detailed description for the popular news article 4",
+            "image": "popular_image_2.jpg",
+            "timeToRead": "7 min",
+            "category": "Business",
+            "categorySlug": "business",
+            "author": "Jane Smith"
+        }
+    ],
+    "mostViewedNews": [
+        {
+            "id": 1,
+            "title": "mostViewedNews News Title 1",
+            "shortDescription": "Short description for the popular news article 1",
+            "loginDescription": "Detailed description for the popular news article 1",
+            "image": "popular_image_1.jpg",
+            "timeToRead": "5 min",
+            "category": "Politics",
+            "categorySlug": "politics",
+            "author": "John Doe"
+        },
+        {
+            "id": 2,
+            "title": "mostViewedNews News Title 2",
+            "shortDescription": "Short description for the popular news article 2",
+            "loginDescription": "Detailed description for the popular news article 2",
+            "image": "popular_image_2.jpg",
+            "timeToRead": "7 min",
+            "category": "Business",
+            "categorySlug": "business",
+            "author": "Jane Smith"
+        },
+        {
+            "id": 3,
+            "title": "mostViewedNews News Title 3",
+            "shortDescription": "Short description for the popular news article 3",
+            "loginDescription": "Detailed description for the popular news article 3",
+            "image": "popular_image_2.jpg",
+            "timeToRead": "7 min",
+            "category": "Business",
+            "categorySlug": "business",
+            "author": "Jane Smith"
+        },
+        {
+            "id": 4,
+            "title": "mostViewedNews News Title 4",
+            "shortDescription": "Short description for the popular news article 4",
+            "loginDescription": "Detailed description for the popular news article 4",
+            "image": "popular_image_2.jpg",
+            "timeToRead": "7 min",
+            "category": "Business",
+            "categorySlug": "business",
+            "author": "Jane Smith"
+        }
+    ],
+    "tagNews": {
+        "technology": [
+            {
+                "id": 1,
+                "title": "mostViewedNews News Title 1",
+                "shortDescription": "Short description for the popular news article 1",
+                "loginDescription": "Detailed description for the popular news article 1",
+                "image": "popular_image_1.jpg",
+                "timeToRead": "5 min",
+                "category": "Politics",
+                "categorySlug": "politics",
+                "author": "John Doe"
+            }
+        ],
+        "sports": [
+            {
+                "id": 1,
+                "title": "mostViewedNews News Title 1",
+                "shortDescription": "Short description for the popular news article 1",
+                "loginDescription": "Detailed description for the popular news article 1",
+                "image": "popular_image_1.jpg",
+                "timeToRead": "5 min",
+                "category": "Politics",
+                "categorySlug": "politics",
+                "author": "John Doe"
+            }
+        ]
+    }
+};
 
-const hotels = require('./data/hotels.json')
-const whyus = require('./data/whyUs.json')
-const destinations = require('./data/destinations.json')
-const tour = require('./data/tour.json')
-const testimonials = require('./data/testimonials.json')
-const blog = require('./data/blog.json')
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Adventura Server Is Running !!! ')
-})
+// Route to get all data
+app.get('/api/data', (req, res) => {
+    res.json(jsonData);
+});
 
 
-app.get('/hotels', (req, res) => {
-    res.send(hotels)
-})
-app.get('/hotels/:id', (req, res) => {
-    const id = req.params.id
-    const hotel = hotels.find(htl => htl.id == id)
-    res.send(hotel)
-})
+// Route to get allNews
+app.get('/api/allNews', (req, res) => {
+    res.json(jsonData.allNews);
+});
 
-app.get('/whyus', (req, res) => {
-    res.send(whyus)
-})
-app.get('/whyus/:id', (req, res) => {
-    const id = req.params.id
-    const whyusd = whyus.find(wu => wu.id == id)
-    res.send(whyusd)
-})
+// Route to get a single item by ID from allNews
+app.get('/api/allNews/:id', (req, res) => {
+    const itemId = parseInt(req.params.id);
+    const item = jsonData.allNews.find((news) => news.id === itemId);
 
-app.get('/destinations', (req, res) => {
-    res.send(destinations)
-})
-app.get('/destinations/:id', (req, res) => {
-    const id = req.params.id
-    const destination = destinations.find(wu => wu.id == id)
-    res.send(destination)
-})
+    if (!item) {
+        res.status(404).json({ error: 'Item not found' });
+    } else {
+        res.json(item);
+    }
+});
+
+// Route to get menu
+app.get('/api/menu', (req, res) => {
+    res.json(jsonData.menu);
+});
+
+// Route to get a single item by ID from menu
+app.get('/api/menu/:id', (req, res) => {
+    const itemId = parseInt(req.params.id);
+    const item = jsonData.menu.find((news) => news.id === itemId);
+
+    if (!item) {
+        res.status(404).json({ error: 'Item not found' });
+    } else {
+        res.json(item);
+    }
+});
+
+// Route to get newsCategories
+app.get('/api/newsCategories', (req, res) => {
+    res.json(jsonData.newsCategories);
+});
+
+// Route to get a single item by ID from newsCategories
+app.get('/api/newsCategories/:id', (req, res) => {
+    const itemId = parseInt(req.params.id);
+    const item = jsonData.newsCategories.find((news) => news.id === itemId);
+
+    if (!item) {
+        res.status(404).json({ error: 'Item not found' });
+    } else {
+        res.json(item);
+    }
+});
+
+// Route to get featuresNews
+app.get('/api/featuresNews', (req, res) => {
+    res.json(jsonData.featuresNews);
+});
+
+// Route to get a single item by ID from featuresNews
+app.get('/api/featuresNews/:id', (req, res) => {
+    const itemId = parseInt(req.params.id);
+    const item = jsonData.featuresNews.find((news) => news.id === itemId);
+
+    if (!item) {
+        res.status(404).json({ error: 'Item not found' });
+    } else {
+        res.json(item);
+    }
+});
 
 
-app.get('/tour', (req, res) => {
-    res.send(tour)
-})
-app.get('/tour/:id', (req, res) => {
-    const id = req.params.id
-    const tours = tour.find(tur => tur.id == id)
-    res.send(tours)
-})
+// Route to get popularNews
+app.get('/api/popularNews', (req, res) => {
+    res.json(jsonData.popularNews);
+});
 
-app.get('/testimonials', (req, res) => {
-    res.send(testimonials)
-})
+// Route to get a single item by ID from popularNews
+app.get('/api/popularNews/:id', (req, res) => {
+    const itemId = parseInt(req.params.id);
+    const item = jsonData.popularNews.find((news) => news.id === itemId);
 
-app.get('/blog', (req, res) => {
-    res.send(blog)
-})
-app.get('/blog/:id', (req, res) => {
-    const id = req.params.id
-    const blogs = blog.find(blg => blg.id == id)
-    res.send(blogs)
-})
+    if (!item) {
+        res.status(404).json({ error: 'Item not found' });
+    } else {
+        res.json(item);
+    }
+});
+
+// Route to get recentNews
+app.get('/api/recentNews', (req, res) => {
+    res.json(jsonData.recentNews);
+});
+
+// Route to get a single item by ID from recentNews
+app.get('/api/recentNews/:id', (req, res) => {
+    const itemId = parseInt(req.params.id);
+    const item = jsonData.recentNews.find((news) => news.id === itemId);
+
+    if (!item) {
+        res.status(404).json({ error: 'Item not found' });
+    } else {
+        res.json(item);
+    }
+});
 
 
+// Route to get mostViewedNews
+app.get('/api/mostViewedNews', (req, res) => {
+    res.json(jsonData.mostViewedNews);
+});
 
+// Route to get a single item by ID from mostViewedNews
+app.get('/api/mostViewedNews/:id', (req, res) => {
+    const itemId = parseInt(req.params.id);
+    const item = jsonData.mostViewedNews.find((news) => news.id === itemId);
+
+    if (!item) {
+        res.status(404).json({ error: 'Item not found' });
+    } else {
+        res.json(item);
+    }
+});
+
+// Route to get tagNews
+app.get('/api/tagNews', (req, res) => {
+    res.json(jsonData.tagNews);
+});
+
+// Route to get a single item by ID from tagNews
+app.get('/api/tagNews/:id', (req, res) => {
+    const itemId = parseInt(req.params.id);
+    const item = jsonData.tagNews.find((news) => news.id === itemId);
+
+    if (!item) {
+        res.status(404).json({ error: 'Item not found' });
+    } else {
+        res.json(item);
+    }
+});
+
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Adventura Server Running On Port ${port}`)
-})
+    console.log(`Server is running on port ${port}`);
+});
